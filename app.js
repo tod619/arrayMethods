b44cceaea675917236bb349e6eea65e9870e6062
tod619 <trevorodwyer619@hotmail.com>
2020-03-13 10:15:28 +0000

    create a function using async await and randomuser api that generates a new user

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
 const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+//Generate Random Users
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+console.log(data);
+
+//fetch random user and add money value
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+  const data = await res.json();
+
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 10000000)
+  };
+
+  addData(newUser);
+}
+
+// Add newUser to data array
+function addData(obj) {
+  data.push(obj);
+}
