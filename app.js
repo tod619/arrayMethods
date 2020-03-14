2ad53d0ab3ca0fbbd340092e9e65ee8596c4848e
tod619 <trevorodwyer619@hotmail.com>
2020-03-14 23:28:43 +0000

    add calculateWealth function to app.js

--- app.js
+++ app.js
@@ -88,6 +88,17 @@ function showMillionaires() {
   updateDOM();
 }
 
+// Calculate the total wealth of all the users
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 // Format number as money
 function formatMoney(number) {
   return "€" + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
@@ -98,3 +109,4 @@ addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
 showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
